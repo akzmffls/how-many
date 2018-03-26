45b213e4fec9c6017e9d8ace2bfa31f7f6973ca8
akzmffls <d7703_14@d7703-14ui-imac.local>
2018-03-26 15:34:49 +0900

    how many

--- how many/ViewController.swift
+++ how many/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var input: UITextField!
+    @IBOutlet var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //deledgate 연결
+        input.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    @IBAction func guess(_ sender: Any) {
+        //랜덤 숫자 생성
+        let number = arc4random() % 6
+        print(number)
+        let numberString = String(number)
+        
+        if input.text == numberString{
+            label.text = "빙고"
+        }else{
+            label.text = "실패"
+        }
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            input.resignFirstResponder()
+            return true
+        }
     }
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {view.endEditing(true)
+        input.backgroundColor = UIColor.cyan
+    }
+    
 
 }
 
